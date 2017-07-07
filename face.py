90f596d33004a768d92b1343c5755f32c9e17f48
mobeets <mobeets@gmail.com>
2017-07-07 11:11:28 -0400

    adding .dat

--- face.py
+++ face.py
@@ -1,14 +1,26 @@
 import os
 import glob
 import tempfile
+import validate_inputs
+from apply_mask import apply_masks
 
-def update_image(infile, url):
-    # f = tempfile.NamedTemporaryFile(suffix='.png', delete=False)
-    # outfile = f.name
-    return infile
+class Struct: pass
+
+def update_image(infile, url=None):
+    f = tempfile.NamedTemporaryFile(suffix='.png', delete=False)
+    outfile = f.name
+
+    args = Struct()
+    args.input = infile
+    args.output = outfile
+    args.mask = 'masks/'
+    args.unique = False
+    input_paths, masks, output_paths, apply_unique_masks, is_video = validate_inputs.run(args)
+    apply_masks(input_paths, masks, output_paths, apply_unique_masks, is_video)
+    return outfile
 
 def test1():
-    pass
+    print update_image('test/face1.jpg')
 
 if __name__ == '__main__':
     test1()
